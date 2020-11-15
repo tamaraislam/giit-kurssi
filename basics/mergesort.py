3b072e64eb5d4f138317e500e24dfd7a60bafbc5
Mohammad Aqib Hassan <mohammad.hassan@tuni.fi>
2020-11-15 04:26:13 +0200

    new mergesort

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,6 @@
+
 def debug_print(debug_msg=None, **kwargs):
+
     if debug_msg:
         print(debug_msg)
 
@@ -43,9 +45,18 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-    quit(1)
+    input_list = input_str.split(",")
+    debug_print(input_list=input_list)
 
-debug_print(value_list=value_list)
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
+            quit(1)
 
-sorted_list = mergesort(value_list)
-print(sorted_list)
+    debug_print(value_list=value_list)
+
+    sorted_list = mergesort(value_list)
+    print(sorted_list)
